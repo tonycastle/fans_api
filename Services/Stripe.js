64f800f556077d395f4c189b7af9b00a145a5673
tonycastle <castleantony6@gmail.com>
2021-10-26 01:29:30 +0200

    moved stripe key to .env file

--- Services/Stripe.js
+++ Services/Stripe.js
@@ -1,5 +1,3 @@
 const Stripe = require("stripe");
 
-exports.stripe = new Stripe(
-  "sk_test_51JWsLSBhgw4Dhe1cWPDvWBARx0FaPSZTYGvXJHfMKwSslA96HcPAGeYeZRlc6jsWy3QOI4c6flEJLGyNhTqMK0Ml00TB32toKn"
-);
+exports.stripe = new Stripe(process.env.STRIPE_KEY);
